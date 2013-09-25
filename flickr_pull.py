f34031c74177d8c7be02081fef1444c08dfb51e8
Evan Hensleigh <futuraprime@gmail.com>
2013-09-24 22:22:16 -0400

    Ok, so we got the files now...

--- flickr_pull.py
+++ flickr_pull.py
@@ -1,5 +1,18 @@
 import flickrapi
 
+class Photo:
+    def __init__(self, photo_xml):
+        attrs = photo_xml.attrib
+        self.id = attrs['id']
+        self.url = attrs['url_m']
+        self.height = int(attrs['height_m'])
+        self.width = int(attrs['width_m'])
+        self.lat = float(attrs['latitude'])
+        self.lng = float(attrs['longitude'])
+
+    def __repr__(self):
+        return '<Photo id:%s lat:%f lng:%f>' % (self.id, self.lat, self.lng)
+
 api_key = '4469375cfadd0f7ad76b2cd06400fb15'
 
 flickr = flickrapi.FlickrAPI(api_key)
@@ -8,11 +21,14 @@ lat = 40.67
 lng = -73.94
 var = 0.5
 
-photos = flickr.photos_search(
-    tags='autumn, tree',
-    bbox=','.join(map(str, [lat - var, lng - var, lat + var, lng + var])),
-    media='photos',
-    extras='url_m',
-)
+def get_photos():
+    photos = flickr.photos_search(
+        tags='autumn, tree',
+        bbox=','.join(map(str, [lat - var, lng - var, lat + var, lng + var])),
+        media='photos',
+        extras='url_m, geo',
+    )
 
-print len(photos.find('photos').findall('photo'))
+    photo_list = [Photo(el) for el in photos.find('photos').findall('photo')]
+
+    return photo_list
